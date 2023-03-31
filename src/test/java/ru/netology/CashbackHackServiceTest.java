d4e6b2004cfc72d39a6a75ba739899d3a63a13df
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-03-31 23:15:39 +0300

    Added Tests

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -7,6 +7,47 @@ import static org.junit.jupiter.api.Assertions.*;
 class CashbackHackServiceTest {
 
     @Test
-    void CashBackHacker() {
+    void amountEqualLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void AmountLessAccrualLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void AmountGreaterAccrualLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void AmountAndOverLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_000_000;
+        int expected = 1000;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void AmountAndUnderLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 300;
+        int expected = 700;
+        int actual = service.remain(amount);
+        assertEquals(expected, actual);
     }
 }
