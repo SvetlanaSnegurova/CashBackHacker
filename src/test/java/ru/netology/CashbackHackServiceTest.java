3bc54cf1f84efdefdc2672792348585f68b1b1d3
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-03-31 23:26:53 +0300

    testng

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,12 +1,53 @@
 package ru.netology;
 
-import org.junit.jupiter.api.Test;
+import org.testng.annotations.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.testng.Assert.assertEquals;
 
-class CashbackHackServiceTest {
+public class CashbackHackServiceTest {
 
     @Test
-    void CashBackHacker() {
+    void amountEqualLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void AmountLessAccrualLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 999;
+        int expected = 1;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void AmountGreaterAccrualLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1001;
+        int expected = 999;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void AmountAndOverLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1_000_000;
+        int expected = 0;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void AmountAndUnderLimit() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 300;
+        int expected = 700;
+        int actual = service.remain(amount);
+        assertEquals(actual, expected);
     }
 }
